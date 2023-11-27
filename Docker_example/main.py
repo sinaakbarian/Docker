392b44a64aceb7a3b4f5648fb0364c6ad2b63ea1
Sina Akbarian <39638602+sinaakbarian@users.noreply.github.com>
2023-11-27 12:26:20 -0500

    Update main.py

--- Docker_example/main.py
+++ Docker_example/main.py
@@ -1,7 +1,6 @@
-# simple ml model
-
 from sklearn.linear_model import LinearRegression
 import numpy as np
+import matplotlib.pyplot as plt
 
 class SimpleLinearModel:
     def __init__(self):
@@ -12,11 +11,63 @@ class SimpleLinearModel:
 
     def predict(self, X):
         return self.model.predict(X)
+
+# Function for input validation with integer values
+def get_int_input(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+            return value
+        except ValueError:
+            print("Invalid input. Please enter a valid integer.")
+
+# Function to collect data for a given number of samples and dimensions
+def collect_data(num_samples, num_dimensions, sample_type):
+    data = []
+    Y=[]
+    print(f"Enter {num_dimensions} space-separated integer values for {sample_type} data.")
+    for j in range(num_samples):
+        print("------------------")
+        x_values_list = [get_int_input(f"Enter x{i+1} for {sample_type} sample {j+1}: ") for i in range(num_dimensions)]
+        data.append(x_values_list)
+        print("------------------")
+        if sample_type=="training":
+            y_value = get_int_input(f"Enter corresponding integer y value for {x_values_list}: ")
+            Y.append(y_value)
+            print("------------------")
+            print(f"Entered {sample_type} sample {j+1}: X = {x_values_list}, y = {y_value}")
+            print("------------------")
+    if sample_type=="training":
+        return data,Y
+    return data
+while(True):
+    # Get number of training samples and dimensions
+    num_train_samples = get_int_input("Enter the number of training samples: ")
+    num_dimensions = get_int_input("Enter the dimensionality of X feature vector: ")
     
-ML_model=SimpleLinearModel()
-X_train=np.array([[2,1],[4,2],[8,3]])
-y_train=np.array([[7],[14],[27]])
-ML_model.fit(X_train,y_train)
-
-
-print(ML_model.predict([[5,7]]))
+    # Collect training data
+    X_train,y_train = collect_data(num_train_samples, num_dimensions, "training")
+    
+    # Train the model
+    ML_model = SimpleLinearModel()
+    ML_model.fit(np.array(X_train), np.array(y_train))
+    
+    # Get number of test samples
+    num_test_samples = get_int_input("Enter the number of test samples: ")
+    
+    # Collect test data
+    X_test = collect_data(num_test_samples, num_dimensions, "test")
+    
+    # Make predictions
+    predictions = ML_model.predict(np.array(X_test))
+    print(f"\nPredictions for test samples: {predictions}\n")
+    
+    # Visualization
+    if num_dimensions == 1:
+        plt.scatter(np.array(X_train), np.array(y_train), color='blue', label='Training Data')
+        plt.scatter(np.array(X_test), predictions, color='green', label='Test Data')
+        plt.plot(np.concatenate([np.array(X_train), np.array(X_test)]), np.concatenate([np.array(y_train), predictions]), color='red', label='Linear Regression Prediction')
+        plt.xlabel('X')
+        plt.ylabel('y')
+        plt.legend()
+        plt.show()
